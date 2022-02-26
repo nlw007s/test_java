9ec5a37ab59d98dd982dc660193d61ddef0687ed
nlw007s <100459295+nlw007s@users.noreply.github.com>
2022-02-26 18:03:09 +0700

    Testing the matrix

--- src/com/tutorial/Main.java
+++ src/com/tutorial/Main.java
@@ -1,8 +1,33 @@
 public class Main{
-    static int x=1;
+
     public static void main(String[] args) {
-        int n=2;
-        System.out.println(x);
-        System.out.println(n);
+        int[][] n = {{1, 2}, {3, 4}};
+        int[][] m = {{5, 6}, {7, 8}};
+        int i, j;
+        
+        int [][] multi = new int[2][2];
+
+        for (i = 0; i < 2; i++) {
+            for (j = 0; j < 2; j++) {
+                System.out.print(" " + n[i][j] + " ");
+            }System.out.println(" ");
+        }System.out.println(" ");
+
+        for (i = 0; i < 2; i++) {
+            for (j = 0; j < 2; j++) {
+                System.out.print(" " + m[i][j] + " ");
+            }System.out.println(" ");
+        }System.out.println(" ");
+
+        for (i = 0; i < 2; i++) {
+            for (j = 0; j < 2; j++) {
+                multi[i][j] = n[i][j] + m[i][j];
+            }
+        }
+        for (i = 0; i < 2; i++) {
+            for (j = 0; j < 2; j++) {
+                System.out.print(" " + multi[i][j] + " ");
+        } System.out.println(" ");
     }
+}
 }
